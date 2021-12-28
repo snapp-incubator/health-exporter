d7d4a813818ed1794c29fe893d132e8fb2b4655d
yeganeahmadnejad <yeganeahmadnejad@gmail.com>
2021-12-28 12:40:54 +0330

    add dns lookup time metric (#14)
    Co-authored-by: Yegane Ahmadnejad <yegane.ahmadnejad@snapp.cab>

--- prober/http.go
+++ prober/http.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"net"
 	"net/http"
+	"net/http/httptrace"
 	url2 "net/url"
 	"strconv"
 	"time"
@@ -29,25 +30,34 @@ var (
 		},
 		[]string{"name", "status_code", "result", "url"},
 	)
+	dnsLookupDurations = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "health_http_dns_lookup_time_seconds",
+			Help:    "The response time of dns lookup",
+			Buckets: []float64{0.001, 0.005, 0.01, 0.02, 0.03, 0.05, 0.075, 0.1, 0.2, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 4, 5},
+		},
+		[]string{"name", "status_code", "dns_error", "result", "url"},
+	)
 )
 
 // TODO: it could be a bad practice!
 func init() {
 	prometheus.MustRegister(httpRequests)
 	prometheus.MustRegister(httpDurations)
+	prometheus.MustRegister(dnsLookupDurations)
+
 }
 
 func NewHttp(name string, url string, rps float64, timeout time.Duration, tlsSkipVerify bool, host string) HTTP {
 	client := &http.Client{
 		Timeout: timeout,
 	}
-
+	customTransport := http.DefaultTransport.(*http.Transport).Clone()
+	customTransport.DisableKeepAlives = true
 	if tlsSkipVerify {
-		customTransport := http.DefaultTransport.(*http.Transport).Clone()
 		customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-
-		client.Transport = customTransport
 	}
+	client.Transport = customTransport
 
 	return HTTP{
 		Name:   name,
@@ -59,10 +69,12 @@ func NewHttp(name string, url string, rps float64, timeout time.Duration, tlsSki
 }
 
 type HTTPResult struct {
-	ResponseTime float64
-	StatusCode   int
-	Error        error
-	ErrorType    string
+	ResponseTime  float64
+	StatusCode    int
+	Error         error
+	ErrorType     string
+	DNSLookupTime float64
+	DNSError string
 }
 
 type HTTP struct {
@@ -112,6 +124,13 @@ func (h *HTTP) Start(ctx context.Context) {
 					"result":      result,
 					"name":        h.Name,
 				}).Observe(stats.ResponseTime)
+				dnsLookupDurations.With(prometheus.Labels{
+					"url":         h.URL,
+					"status_code": strconv.Itoa(stats.StatusCode),
+					"result":      result,
+					"name":        h.Name,
+					"dns_error":   stats.DNSError,
+				}).Observe(stats.DNSLookupTime)
 			})()
 
 		}
@@ -123,30 +142,52 @@ func (h *HTTP) calculateInterval() time.Duration {
 }
 
 func (h *HTTP) sendRequest(ctx context.Context) HTTPResult {
-	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, h.URL, nil)
-	if h.Host != "" {
-		req.Host = h.Host
+	var startTime time.Time
+	var finishTime time.Time
+	var dnsStartTime time.Time
+	var dnsDoneTime time.Time
+	var dnsError string
+	httpTrace := &httptrace.ClientTrace{
+		DNSStart: func(_ httptrace.DNSStartInfo) {
+			dnsStartTime = time.Now()
+		},
+		DNSDone: func(info httptrace.DNSDoneInfo) {
+			dnsDoneTime = time.Now()
+			if info.Err != nil {
+				klog.Infof("DNS Error: %v ", info.Err)
+				dnsError = info.Err.Error()
+			}
+		},
 	}
-	start := time.Now()
+
+	req, _ := http.NewRequest(http.MethodGet, h.URL, nil)
+	clientTraceCtx := httptrace.WithClientTrace(req.Context(), httpTrace)
+	req = req.WithContext(clientTraceCtx)
+
+	startTime = time.Now()
 	res, err := h.Client.Do(req)
-	responseTime := time.Since(start).Seconds()
+	finishTime = time.Now()
+
+	responseTime := finishTime.Sub(startTime)
+	dnsLookupTime := dnsDoneTime.Sub(dnsStartTime)
 
 	if err != nil {
 		return HTTPResult{
-			ResponseTime: responseTime,
-			Error:        err,
-			ErrorType:    h.errorType(err),
+			ResponseTime:  float64(responseTime),
+			Error:         err,
+			ErrorType:     h.errorType(err),
+			DNSLookupTime: float64(dnsLookupTime),
+			DNSError: dnsError,
 		}
 	}
-
 	defer res.Body.Close()
-
 	return HTTPResult{
-		StatusCode:   res.StatusCode,
-		ResponseTime: responseTime,
+		StatusCode:    res.StatusCode,
+		ResponseTime:  float64(responseTime),
+		DNSLookupTime: float64(dnsLookupTime),
+		DNSError: dnsError,
 	}
 }
-
 func (h *HTTP) errorType(err error) string {
 	if timeoutError, ok := err.(net.Error); ok && timeoutError.Timeout() {
 		return "timeout"
